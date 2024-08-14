9e62b32dfd02d9a6149b2456d10bfe17d2e4a2bb
Komal <komal.ayare@oracle.com>
2024-08-14 15:13:18 +0530

    ActivitySol

---
+++ activity-01/employee.js
@@ -0,0 +1,12 @@
+export class Employee {
+    constructor(id, name, salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    display() {
+        //console.log("Id: "+this.id, "Name: "+this.name, "Salary:" +this.salary);
+        console.log(`Id: ${this.id}, Name: ${this.name}, Salary: ${this.salary}`);
+    }
+}
